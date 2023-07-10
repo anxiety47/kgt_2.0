3da5db1d3728bf5ca3571f00587b6bf76b57262a
Katarzyna Kalota <anxiety477@gmail.com>
2023-07-10 23:07:07 +0200

    fix displaying training map poc for angular google map package

--- src/app/trainings/training-map/training-map.component.ts
+++ src/app/trainings/training-map/training-map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { points, pointsBlue } from './points';
 
 @Component({
   selector: 'app-training-map',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class TrainingMapComponent {
 
+  points = points;
+  myLatLng = { lat: 50.01146223876953, lng: 19.82978582382202 };
+  mapOptions: google.maps.MapOptions = {
+    center: this.myLatLng,
+    zoom: 20,
+  };
+
+  iconSize = 6;
+  icon = {
+    url: "/assets/images/dot10px.svg",
+    scaledSize: new google.maps.Size(this.iconSize, this.iconSize),
+    anchor: new google.maps.Point(this.iconSize/2, this.iconSize/2)
+  }
+
+  markerOptions: google.maps.MarkerOptions = {icon: this.icon};
+
+  polylineOptions = {
+    path: points,
+    strokeColor: '#ff0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+    };
+
+  polylineOptionsBlue = {
+    path: pointsBlue,
+    strokeColor: '#0000ff',
+    strokeOpacity: 1.0,
+    strokeWeight: 4,
+  };
 }
--- src/app/trainings/trainings.module.ts
+++ src/app/trainings/trainings.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingsListComponent } from './trainings-list/trainings-list.component';
 import { TrainingMapComponent } from './training-map/training-map.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { TrainingMapComponent } from './training-map/training-map.component';
     TrainingsListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    GoogleMapsModule
   ]
 })
 export class TrainingsModule { }
