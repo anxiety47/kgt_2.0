3b6c1d45187e2cab5ecb8881761424a3d2a0d157
Tomasz Kleiner <tomasz.kleiner@gmail.com>
2023-10-13 21:39:49 +0200

    Add Data project from previous version of the application

---
+++ backend/KGT.Data/DataTransferObjects/Enums/DogLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace KGT.Data.DataTransferObjects.Enums
+{
+    public enum DogLevel
+    {
+        Nowy,
+        Początkujący,
+        Średniozaawanowany,
+        Zaawansowany,
+        Ekspert
+    }
+}
---
+++ backend/KGT.Data/Models/Enums/DogLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace KGT.Data.Models.Enums
+{
+    public enum DogLevel
+    {
+        NewRecruit,
+        Beginner,
+        Intermediate,
+        Advanced,
+        Expert
+    }
+}
