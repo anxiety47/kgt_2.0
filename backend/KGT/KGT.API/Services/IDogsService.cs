f2176e01c4a3d989a35ed56affd58292120eae84
Tomasz Kleiner <tomasz.kleiner@gmail.com>
2024-05-11 00:16:49 +0200

    Refactor and add new dog to cosmosdb

--- backend/KGT/KGT.API/Services/IDogsService.cs
+++ backend/KGT/KGT.API/Services/IDogsService.cs
@@ -6,6 +6,8 @@ namespace KGT.API.Services
     {
         public Task<List<DogBasicInfo>> GetAllDogsBasicInfo();
         public Task<DogDetails> GetDogDetails(Guid id);
-
+        public Task<DogDetails> AddDog(AddNewDog newDog);
+        public Task<DogDetails> UpdateDog(DogDetails dogDetails);
+        public Task DeleteDog(Guid id);
     }
 }
---
+++ backend/KGT/KGT.Data/CosmosDb/DogsRepository.cs
@@ -0,0 +1,67 @@
+﻿using KGT.Data.Models.Dogs;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KGT.Data.CosmosDb
+{
+
+    public class DogsRepository : BaseCosmosService, IDogsRepository
+    {
+
+        public DogsRepository(CosmosClient cosmosClient, IConfiguration configuration) : base(cosmosClient, configuration)
+        {
+        }
+
+        private Container _container
+        {
+            get => base.GetContainerByName("Dogs");
+        }
+
+
+        public async Task<IEnumerable<Dog>> GetDogsAsync()
+        {
+            var query = _container.GetItemQueryIterator<Dog>(new QueryDefinition("SELECT * FROM c"));
+            var results = new List<Dog>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+            return results;
+        }
+
+        public async Task<Dog> GetDogAsync(Guid id)
+        {
+            try
+            {
+                var response = await _container.ReadItemAsync<Dog>(id.ToString(), new PartitionKey(id.ToString()));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Dog> CreateDogAsync(Dog dog)
+        {
+            var response = await _container.CreateItemAsync(dog, new PartitionKey(dog.Id.ToString()));
+            return response.Resource;
+        }
+
+        public async Task<Dog> UpdateDogAsync(Guid id, Dog dog)
+        {
+            var response = await _container.UpsertItemAsync(dog, new PartitionKey(id.ToString()));
+            return response.Resource;
+        }
+
+        public async Task DeleteDogAsync(Guid id)
+        {
+            await _container.DeleteItemAsync<Dog>(id.ToString(), new PartitionKey(id.ToString()));
+        }
+    }
+}
